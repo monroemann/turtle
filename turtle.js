2741d552ec95d787fee8f1bdcaeefc5a783fb510
Monroe Mann <findingsummer@aol.com>
2020-09-01 13:02:04 -0400

    Added a safe zone

--- turtle.js
+++ turtle.js
@@ -9,7 +9,7 @@ let rotate = 0;
 
 function handleKeyDown(event) {
 
-	//if not an arrow key, we ignore key press
+	// if not an arrow key, we ignore key press
 	if(!event.key.includes('Arrow')) {
 		return;
 	}
@@ -46,9 +46,28 @@ function handleKeyDown(event) {
 		--x: ${x * speed}px; 
 		--y: ${y * speed}px;
 	`);	
+	console.log(x, y);
+	isSafe(x, y);
+}
 
-	// console.log(x, y);
+function changeSafeZoneColor(event) {
+	safeZone.style.backgroundColor = "black";
+}
+
+//this is pseudocode.  The .position method call is just something I made up
+function isSafe(x, y) {
+	if(x === 6 && y === 0) {
+		safeZone.style.backgroundColor = "blue";
+		console.log("Yes, you're safe!");
+	} 
+	else {
+		safeZone.style.backgroundColor = null;
+		console.log("You're in danger!")
+	}
+
 }
 
 window.addEventListener('keydown', handleKeyDown);
 
+var safeZone = document.querySelector('.safe-zone');
+safeZone.addEventListener('mouseover', changeSafeZoneColor);
